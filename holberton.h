30de4a3221b60b372dd462f88ac7cc31c5f2b4b4
Yakufaith <jaynjaycob@gmail.com>
2021-07-27 15:01:28 +0200

    File containing all function prototypes has being created

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _printf(const char *format, ...);
+void print_b(int num, int *count_ptr);
+void print_d(int num, int *count_ptr);
+void print_o(int num, int *count_ptr, int tmp);
+int _putchar(char c);
+void print_X(unsigned int num, int *count_ptr, int tmp);
+void print_x(unsigned int num, int *count_ptr, int tmp);
+void print_u(unsigned int num, int *count_ptr);
+void print_str(char *str, int *count_ptr);
+
+#endif
